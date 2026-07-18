0f5d067f36d6a9a07cfa4e2548e22c0a5690e966
LoganRilling <loganrilling@gmail.com>
2026-07-18 17:35:57 -0400

    Removed the Vector class. Made a clearer method for getting a rotational quaternion and getting angles.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-mod vector;
 mod quaternion;
-
-use vector::Vector;
 use quaternion::Quaternion;
 
 use std::f64::consts::PI;
 
-fn display_vector(v: Vector)
+
+fn rad_to_deg(degrees: f64) -> f64
 {
-    for i in 0..(v.dims.len())
-    {
-        let val = v.dims[i];
-        println!("Dimension {i}: {val}");
-    }
+    return 180.0 * degrees / PI;
+}
+
+fn deg_to_rad(radians: f64) -> f64
+{
+    return PI * radians / 180.0;
 }
 
 fn display_quaternion(q: Quaternion)
@@ -28,7 +27,31 @@ fn display_quaternion(q: Quaternion)
     println!("z: {z}");
 }
 
+fn display_quaternion_deg(q: Quaternion)
+{
+    let w = rad_to_deg(q.w);
+    let x = rad_to_deg(q.x);
+    let y = rad_to_deg(q.y);
+    let z = rad_to_deg(q.z);
+
+    println!("w: {w}");
+    println!("x: {x}");
+    println!("y: {y}");
+    println!("z: {z}");
+}
+
 fn main() 
 {
-    
+    let q0 = Quaternion::new_from_pitch(deg_to_rad(90.0));
+    let q1 = Quaternion::new_from_pitch(deg_to_rad(180.0));
+    let p = q0.get_rotation_to(&q1);
+
+    let a0 = rad_to_deg(q0.get_angle());
+    let a1 = rad_to_deg(q1.get_angle());
+    //let ap = rad_to_deg(p.get_angle());
+    let o = rad_to_deg((q0 * p).get_angle());
+
+    println!("q_0 pitch: {a0}");
+    println!("q_1 pitch: {a1}");
+    println!("q0 * p = q1 pitch: {o}");
 }
--- src/quaternion.rs
+++ src/quaternion.rs
@@ -1,5 +1,4 @@
-use crate::vector::{Vector, VectorT};
-use std::ops::Mul;
+use std::ops::{Mul, Add};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Quaternion
@@ -7,9 +6,11 @@ pub struct Quaternion
     pub w: f64,
     pub x: f64,
     pub y: f64,
-    pub z: f64
+    pub z: f64,
+    unit_quaternion: bool
 }
 
+#[allow(dead_code)]
 impl Quaternion
 {
     pub fn new(w: f64, x: f64, y: f64, z: f64) -> Quaternion
@@ -19,45 +20,58 @@ impl Quaternion
             w,
             x,
             y,
-            z
+            z,
+            unit_quaternion: false
         };
     }
 
-    pub fn new_from_vector(v: &Vector) -> Result<Quaternion, &'static str>
+    
+    /// Creates a quaternion and enables the unit quaternion property on by default.
+    /// 
+    /// A unit quaternion ensures that the magnitude of the quaternion is always 1, 
+    /// no matter the operations conducted onto it. It simply saves computational headaches.
+    pub fn new_unit_quat(w: f64, x: f64, y: f64, z: f64) -> Quaternion
     {
-        if v.dims.len() != 3
+        return Quaternion 
         {
-            return Err("Can only create a quaternion from a vector of exactly 3 dimensions!");
-        }
-
-        return Ok(Quaternion 
-        {
-            w: 0.0,
-            x: v.dims[0],
-            y: v.dims[1],
-            z: v.dims[2]
-        });
+            w,
+            x,
+            y,
+            z,
+            unit_quaternion: true
+        }.normalize().expect("Normalized quaternion");
     }
 
-    /// Rotation over x-axis
+    /// Sets the unit quaternion property on/off
+    pub fn set_unit_quat(mut self, state: bool)
+    {
+        self.unit_quaternion = state;
+
+        if self.unit_quaternion
+        {
+            self = self.normalize().expect("Normalized quaternion");
+        }
+    }
+
+    /// Unit quaternion that represents a rotation over x-axis
     pub fn new_from_roll(angle_rad: f64)  -> Quaternion
     {
-        return Self::new((angle_rad * 0.5).cos(), (angle_rad * 0.5).sin(), 0.0, 0.0);
+        return Self::new_unit_quat((angle_rad * 0.5).cos(), (angle_rad * 0.5).sin(), 0.0, 0.0);
     }
 
-    /// Rotation over y-axis
+    /// Unit quaternion that represents a rotation over y-axis
     pub fn new_from_pitch(angle_rad: f64)  -> Quaternion 
     {
-        return Self::new((angle_rad * 0.5).cos(),0.0, (angle_rad * 0.5).sin(), 0.0);
+        return Self::new_unit_quat((angle_rad * 0.5).cos(),0.0, (angle_rad * 0.5).sin(), 0.0);
     }
 
-    /// Rotation over z-axis
+    /// Unit quaternion that represents a rotation over z-axis
     pub fn new_from_yaw(angle_rad: f64)  -> Quaternion
     {
-        return Self::new((angle_rad * 0.5).cos(), 0.0, 0.0, (angle_rad * 0.5).sin());
+        return Self::new_unit_quat((angle_rad * 0.5).cos(), 0.0, 0.0, (angle_rad * 0.5).sin());
     }
 
-    /// Creates a quaternion from Euler angles using the
+    /// Creates a unit quaternion from Euler angles using the
     /// ZYX (yaw → pitch → roll) convention.
     pub fn new_from_euler(yaw_z_rad: f64, pitch_y_rad: f64, roll_x_rad: f64) -> Quaternion
     {
@@ -92,46 +106,29 @@ impl Quaternion
         )
     }
 
-    pub fn get_inverse(&self) -> Result<Quaternion, &'static str> 
+    /// Creates a transitional or error quaternion. 
+    /// Gets the differential quaternion components to be used for a rotation
+    pub fn get_rotation_to(&self, target: &Quaternion) -> Quaternion
     {
-        let conj = self.get_conjugate();
-        let mag = self.get_magnitude();
+        let q0 = self.clone();
+        let q1 = target.clone();
 
-        if mag == 0.0
+        if !q0.unit_quaternion
         {
-            return Err("Can not divide by zero!");
+            q0.set_unit_quat(true);
         }
 
-        return Ok(Quaternion::new(
-            conj.w / (mag * mag),
-            conj.x / (mag * mag),
-            conj.y / (mag * mag),
-            conj.z / (mag * mag),
-        ));
-    }
+        if !q1.unit_quaternion
+        {
+            q1.set_unit_quat(true);
+        }
 
-    pub fn get_rotation_error(&self, target: &Quaternion) -> Quaternion
-    {
-        return target.normalize().unwrap() * self.normalize().unwrap().get_conjugate();
-    }
-
-    pub fn rotate(&mut self, target: &Quaternion)
-    {
-        let mut mag = self.get_magnitude();
-
-        *self = mag * (target.normalize().unwrap() * self.normalize().unwrap());
-    }
-
-    pub fn to_euler_as_vector(&self) -> Vector
-    {
-        let (x, y, z) = self.to_euler();
-
-        return Vector::new(vec!(x, y, z));
+        return q1.hamilton_product(q0.get_conjugate());
     }
 
     pub fn hamilton_product(&self, other: Quaternion) -> Quaternion
     {
-        return Self::new(
+        let q = Self::new(
             (self.w * other.w) -
                 (self.x * other.x) -
                 (self.y * other.y) -
@@ -140,6 +137,53 @@ impl Quaternion
             (self.w * other.y) + (other.w * self.y) - ((self.x * other.z) - (self.z * other.x)),
             (self.w * other.z) + (other.w * self.z)+ (self.x * other.y) - (self.y * other.x),
         );
+
+        if self.unit_quaternion
+        {
+            return q.normalize().expect("Return normalized hamilton_product");
+        }
+        else 
+        {
+            return q;    
+        }
+    }
+
+    pub fn dot(&self, other: &Quaternion) -> f64
+    {
+        return (self.w * other.w) + (self.x * other.x) + (self.y * other.y) + (self.z * other.z);
+    }
+
+    pub fn add(&self, other: &Quaternion) -> Quaternion
+    {
+        return Self::new(self.w + other.w, self.x + other.x, self.y + other.y, self.z + other.z);
+    }
+
+    pub fn subtract(&self, other: &Quaternion) -> Quaternion
+    {
+        return Self::new(self.w - other.w, self.x - other.x, self.y - other.y, self.z - other.z);
+    }
+
+    pub fn get_magnitude(&self) -> f64
+    {
+        return Self::dot(&self, self).sqrt();
+    }
+
+    pub fn normalize(&self) -> Result<Quaternion, &'static str>
+    {
+        let mag = self.get_magnitude();
+
+        if mag == 0.0
+        {
+            return Err("Can not normalize a quaternion of magnitude 0.");
+        }
+
+        return Ok(Self::new(self.w/mag, self.x/mag, self.y/mag, self.z/mag));
+    }
+
+    /// Gets the current angle in radians
+    pub fn get_angle(&self) -> f64
+    {
+        return 2.0 * self.w.acos();
     }
 }
 
@@ -153,6 +197,18 @@ impl Mul for Quaternion
     }
 }
 
+#[allow(unconditional_recursion)]
+impl Add for Quaternion
+{
+    type Output = Quaternion;
+
+    fn add(self, rhs: Self) -> Self::Output 
+    {
+        return self.add(rhs);
+    }
+}
+
+/// Scalar multiplicaton for quaternions
 impl Mul<Quaternion> for f64
 {
     type Output = Quaternion;
@@ -165,48 +221,5 @@ impl Mul<Quaternion> for f64
             rhs.y * self,
             rhs.z * self,
         )
-    }
-}
-
-impl VectorT for Quaternion
-{
-    type Output = Quaternion;
-
-    fn dot(&self, other: &Quaternion) -> Result<f64, &'static str>
-    {
-        return Ok((self.w * other.w) + (self.x * other.x) + (self.y * other.y) + (self.z * other.z));
-    }
-
-    fn add(&self, other: &Quaternion) -> Result<Quaternion, &'static str>
-    {
-        return Ok(Self::new(self.w + other.w, self.x + other.x, self.y + other.y, self.z + other.z));
-    }
-
-    fn subtract(&self, other: &Quaternion) -> Result<Quaternion, &'static str>
-    {
-        return Ok(Self::new(self.w - other.w, self.x - other.x, self.y - other.y, self.z - other.z));
-    }
-
-    fn get_magnitude(&self) -> f64
-    {
-        return Self::dot(&self, self).unwrap().sqrt();
-    }
-
-    fn normalize(&self) -> Result<Quaternion, &'static str>
-    {
-        let mag = self.get_magnitude();
-
-        if mag == 0.0
-        {
-            return Err("Can not normalize a quaternion of magnitude 0.");
-        }
-
-        return Ok(Self::new(self.w/mag, self.x/mag, self.y/mag, self.z/mag));
-    }
-
-    fn get_angle_rad(&self, other: &Quaternion) -> f64
-    {
-        return (Self::dot(self, other).unwrap() /
-        (self.get_magnitude() * other.get_magnitude())).acos();
     }
 }
